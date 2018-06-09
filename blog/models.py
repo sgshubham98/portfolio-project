0e1d57201f8224a3b776e8f3a48940f3b766a00d
Shubham Goswami <sgshubham98@gmail.com>
2018-06-09 14:47:57 +0530

    BlogFunctionality

--- blog/models.py
+++ blog/models.py
@@ -7,3 +7,11 @@ class Blog(models.Model):
 	body = models.TextField()
 	image = models.ImageField(upload_to='images/')
 
+	def __str__(self):
+		return self.title
+
+	def summary(self):
+		return self.body[:60]
+
+	def pub_date_good(self):
+			return self.pub_date.strftime('%b %e %Y')
